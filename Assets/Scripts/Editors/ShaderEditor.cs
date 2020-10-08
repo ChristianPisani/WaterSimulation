9f725474eebac851f3df74224c8fafbeb4011646
Christian Thorvik <christian96-th@hotmail.com>
2020-10-08 20:43:49 +0200

    Converted FFT C# code to compute shader

--- Assets/Scripts/Editors/ShaderEditor.cs
+++ Assets/Scripts/Editors/ShaderEditor.cs
@@ -24,6 +24,15 @@ class TessendorfEditor : Editor {
     {
         DrawDefaultInspector();
 
+        GUILayout.Label("CPU Textures");
+        Texture2D t = AssetPreview.GetAssetPreview(((Tessendorf)target).H0KTexCPU);
+        GUILayout.Label(t);
+        Texture2D t1 = AssetPreview.GetAssetPreview(((Tessendorf)target).H0NegativeKTexCPU);
+        GUILayout.Label(t1);
+        Texture2D t2 = AssetPreview.GetAssetPreview(((Tessendorf)target).TimedependentHTexCPU);
+        GUILayout.Label(t2);
+
+        GUILayout.Label("GPU Textures");
         Texture2D tex = AssetPreview.GetAssetPreview(((Tessendorf)target).H0KTex);
         GUILayout.Label(tex);
         Texture2D tex1 = AssetPreview.GetAssetPreview(((Tessendorf)target).H0NegativeKTex);
@@ -31,9 +40,23 @@ class TessendorfEditor : Editor {
         Texture2D tex2 = AssetPreview.GetAssetPreview(((Tessendorf)target).TimedependentHTex);
         GUILayout.Label(tex2);
 
-        if (GUILayout.Button("Visalize noise"))
+        GUILayout.Label("Gaussian noise texture");
+        Texture2D noiseTex = AssetPreview.GetAssetPreview(((Tessendorf)target).NoiseTexture);
+        GUILayout.Label(noiseTex);
+
+        if (GUILayout.Button("Visalize noise CPU"))
         {
-            ((Tessendorf)target).VisualizeNoise();
+            ((Tessendorf)target).VisualizeNoiseCpu();
+        }
+
+        if (GUILayout.Button("Visalize noise GPU"))
+        {
+            ((Tessendorf)target).VisualizeNoiseGpu();
+        }
+
+        if (GUILayout.Button("Setup noisetexture"))
+        {
+            ((Tessendorf)target).CreateGaussianNoiseTexture();
         }
     }
 }
--- Assets/Scripts/Tessendorf.cs
+++ Assets/Scripts/Tessendorf.cs
@@ -13,18 +13,26 @@ namespace Assets.Scripts {
         public float Depth = 10;
         public int N = 256;
 
-        public Texture2D H0KTex;
-        public Texture2D H0NegativeKTex;
-        public Texture2D TimedependentHTex;
-        
+        public Texture2D H0KTexCPU;
+        public Texture2D H0NegativeKTexCPU;
+        public Texture2D TimedependentHTexCPU;
+
+        public RenderTexture H0KTex;
+        public RenderTexture H0NegativeKTex;
+        public RenderTexture TimedependentHTex;
+
+        public Texture2D NoiseTexture;
+
         private FourierDomain _fftFunctions = new FourierDomain();
-        
-        public void VisualizeNoise()
+
+        public ComputeShader FFTComputeShader;
+
+        public void VisualizeNoiseCpu()
         {
-            H0KTex = new Texture2D(N, N);
-            H0NegativeKTex = new Texture2D(N, N);
-            TimedependentHTex = new Texture2D(N, N);
-            
+            H0KTexCPU = new Texture2D(N, N);
+            H0NegativeKTexCPU = new Texture2D(N, N);
+            TimedependentHTexCPU = new Texture2D(N, N);
+
             for (int x = 0; x < N; x++)
             {
                 for (int y = 0; y < N; y++)
@@ -35,15 +43,69 @@ namespace Assets.Scripts {
                     var h0NegK = _fftFunctions.FourierAmplitudeFirst(Amplitude, -waveDirection, WindForce, WindDirection);
                     var hkt = _fftFunctions.FourierAmplitude(h0k, h0NegK, Amplitude, waveDirection, WindForce, WindDirection, Depth, Time.time);
 
-                    H0KTex.SetPixel(x, y, new Color((float)h0k.Real, (float)h0k.Imaginary, 0, 1));
-                    H0NegativeKTex.SetPixel(x, y, new Color((float)h0NegK.Real, (float)h0NegK.Imaginary, 0, 1));
+                    H0KTexCPU.SetPixel(x, y, new Color((float)h0k.Real, (float)h0k.Imaginary, 0, 1));
+                    H0NegativeKTexCPU.SetPixel(x, y, new Color((float)h0NegK.Real, (float)h0NegK.Imaginary, 0, 1));
 
-                    TimedependentHTex.SetPixel(x, y, new Color((float)hkt.Real, (float)hkt.Imaginary, 0, 1));
+                    TimedependentHTexCPU.SetPixel(x, y, new Color((float)hkt.Real, (float)hkt.Imaginary, 0, 1));
                 }
             }
-            H0KTex.Apply();
-            H0NegativeKTex.Apply();
-            TimedependentHTex.Apply();
-        }  
+            H0KTexCPU.Apply();
+            H0NegativeKTexCPU.Apply();
+            TimedependentHTexCPU.Apply();
+        }
+
+        public void VisualizeNoiseGpu()
+        {
+            if (NoiseTexture == null) return;
+
+            H0KTex = new RenderTexture(N, N, 1);
+            H0NegativeKTex = new RenderTexture(N, N, 1);
+            TimedependentHTex = new RenderTexture(N, N, 1);
+
+            H0KTex.enableRandomWrite = true;
+            H0NegativeKTex.enableRandomWrite = true;
+            TimedependentHTex.enableRandomWrite = true;
+
+            H0KTex.Create();
+            H0NegativeKTex.Create();
+            TimedependentHTex.Create();
+
+            int kernel = FFTComputeShader.FindKernel("CSMain");
+
+            FFTComputeShader.SetTexture(kernel, "H0KTexture", H0KTex);
+            FFTComputeShader.SetTexture(kernel, "H0NegKTexture", H0NegativeKTex);
+            FFTComputeShader.SetTexture(kernel, "NoiseTexture", NoiseTexture);
+            FFTComputeShader.SetFloat("_Time", Time.time);
+            FFTComputeShader.SetFloat("_Amplitude", Amplitude);
+            FFTComputeShader.SetFloat("_WindForce", WindForce);
+            FFTComputeShader.SetVector("_WindDirection", WindDirection);
+            FFTComputeShader.SetFloat("_Depth", Depth);
+            FFTComputeShader.SetInt("_Resolution", N);
+
+            FFTComputeShader.Dispatch(kernel, N / 8, N / 8, 1);
+        }
+
+        public void CreateGaussianNoiseTexture()
+        {
+            NoiseTexture = new Texture2D(N, N);
+
+            float mean = 0;
+            float stdDev = 1;
+
+            for (int x = 0; x < N; x++)
+            {
+                for (int y = 0; y < N; y++)
+                {
+                    NoiseTexture.SetPixel(x, y,
+                        new Color(
+                            (float)NumberDistributions.GaussianRandom(mean, stdDev),
+                            (float)NumberDistributions.GaussianRandom(mean, stdDev),
+                            (float)NumberDistributions.GaussianRandom(mean, stdDev),
+                            (float)NumberDistributions.GaussianRandom(mean, stdDev)
+                        ));
+                }
+            }
+            NoiseTexture.Apply();
+        }
     }
 }
